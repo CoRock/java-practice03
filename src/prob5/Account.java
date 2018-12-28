943a83e4246378e5a66f95d90490c171f023dab0
CoRock <dycorphin@gmail.com>
2018-12-28 19:22:14 +0900

    Add solving problem 5

--- src/prob5/Account.java
+++ src/prob5/Account.java
@@ -1,4 +1,55 @@
 package prob5;
 
 public class Account {
+
+	private String accountNo;
+	private int balance;
+
+	public Account() {
+		this(null, 0);
+	}
+	
+	public Account(String accountNo) {
+		this(accountNo, 0);
+	}
+
+	public Account(String accountNo, int balance) {
+		this.accountNo = accountNo;
+		this.balance = balance;
+		System.out.println(accountNo + " 계좌가 개설되었습니다.");
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public void save(int money) {
+		if (money < 0) {
+			money = 0;
+		}
+		balance += money;
+		System.out.println(accountNo + " 계좌에 " + balance + "만원이 입금되었습니다.");
+	}
+	
+	public void deposit(int money) {
+		if (balance < money) {
+			System.out.println("잔액이 부족합니다. 다시 시도해주세요.");
+			return;
+		}
+		balance -= money;
+		System.out.println(accountNo + " 계좌에 " + money + "만원이 출금되었습니다.");
+	}
+
 }
