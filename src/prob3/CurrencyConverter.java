f979a4b8fe12ed42d5a696cd145ef0838504e00d
CoRock <dycorphin@gmail.com>
2018-12-28 19:27:50 +0900

    Add solving problem 3

--- src/prob3/CurrencyConverter.java
+++ src/prob3/CurrencyConverter.java
@@ -1,4 +1,25 @@
 package prob3;
 
 public class CurrencyConverter {
+	
+	private static double rate;
+
+	public static double toDollar(double won) {
+		// TODO: 한국 원화를 달러로 변환
+		double dollar = won / CurrencyConverter.rate;
+		
+		return dollar;
+	}
+
+	public static double toKRW(double dollar) {
+		// TODO: 달러를 한국 원화로 변환
+		double krw = dollar * CurrencyConverter.rate;
+		return krw;
+	}
+	
+	public static void setRate(double r) {
+		// TODO: 환율 설정(KRW/$1)
+		CurrencyConverter.rate = r;
+	}
+	
 }
--- src/prob3/Prob3.java
+++ src/prob3/Prob3.java
@@ -6,7 +6,7 @@ public class Prob3 {
 		CurrencyConverter.setRate( 1118.70 );
 		
 		double dollar = CurrencyConverter.toDollar( 1000000 );
-		System.out.println( "백만원은 " + dollar + "달러입니다." );
+		System.out.println( "백만원은 " + dollar + "달러 입니다." );
 		
 		double krw = CurrencyConverter.toKRW( 100 );
 		System.out.println( "백달러는 " + krw + "원 입니다." );		
