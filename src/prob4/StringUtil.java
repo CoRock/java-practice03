ea7a946133b3c6e483a112b65fad7f4e3de4cabf
CoRock <dycorphin@gmail.com>
2018-12-28 17:10:01 +0900

    Add solving problem 4 (prob4 package)

--- src/prob4/StringUtil.java
+++ src/prob4/StringUtil.java
@@ -2,4 +2,15 @@ package prob4;
 
 public class StringUtil {
 
+    public static String concatenate(String[] str) {
+        //문자열을 결합하여 리턴 하는 메소드 구현
+    	String strSet = "";
+    	
+    	for (String s : str) {
+    		strSet += s;
+    	}
+    	
+    	return strSet;
+    }
+
 }
